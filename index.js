ca455e0f00248bcb1ae302b09f44bae9b2b0289f
Coriana Williams <corianacory@gmail.com>
2023-05-23 00:12:20 -0400

    made edits

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 'use strict';
 
 require('dotenv').config();
-const { startServer } = require('./server');
+const { sequelizeDatabase } = require('./src/auth/models/index');
+const { start } = require('./src/server');
 
-// Connect to the database
-// Add any necessary database configuration here
+const PORT = process.env.PORT || 3002;
 
-// Start the server
-startServer();
+// make sure our tables are created, start up the HTTP server.
+sequelizeDatabase.sync()
+  .then(() => {
+    console.log('Connection is working!');
+    start(PORT);
+  }).catch(e => {
+    console.error('Could not start server', e.message);
+  });
